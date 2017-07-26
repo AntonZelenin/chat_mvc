165917c9efb4e29918128310f8f4dd92ee923e34
Anton Litvinenko <dev.litvinenko@gmail.com>
2017-07-26 13:51:08 +0300

    commit

--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,33 @@
+// preloader
+$(window).on('load', function() { 
+	$('.loader-inner').fadeOut(); 
+	$('.loader').delay(400).fadeOut('slow'); 
+});
+
+
 $(function() {
 
+// autoresize textarea
+$('#message-field').autoResize({
+	extraSpace : 11
+});
+
+
+// open-close sidebar
 $('.js-toggle-btn').on('click', function() {
-	$(this).toggleClass('on');
-	$('.js-user-list').toggleClass('user-list--narrow');
-	$('.js-post-list').toggleClass('post-list--wide');
-	// $('.main-menu').slideToggle();
+	if ($(window).width() <= '480') {
+		$('.js-user-list').removeClass('show');
+		$(this).removeClass('on');
+	} else {
+		$(this).toggleClass('on');
+		$('.js-user-list').toggleClass('user-list--narrow');
+	}
+	return false;
+});
+
+$('.js-toggle-btn--fixed').on('click', function() {
+	$('.js-user-list').toggleClass('show');
+	$('.js-toggle-btn').toggleClass('on');
 	return false;
 });
 
@@ -15,20 +38,21 @@ $(window).resize(function(){
 narrowMenu();
 
 function narrowMenu() {
-	if($(window).width() <= '768') {
+	if ($(window).width() <= '480') {
+		$('.js-user-list').removeClass('user-list--narrow show');
+	} 
+	else if ($(window).width() > '480' && $(window).width() <= '768'  ) {
 		$('.js-user-list').addClass('user-list--narrow');
-		$('.js-post-list').addClass('post-list--wide');
 		$('.js-toggle-btn').removeClass('on');
 	}
 	else {
 		$('.js-user-list').removeClass('user-list--narrow');
-		$('.js-post-list').removeClass('post-list--wide');
 		$('.js-toggle-btn').addClass('on');
-		// 	$('.main-menu').removeAttr('style');
 	}
 }
 
 
+// open-close searchform
 $('.js-search-btn-post').on('click', function(){
 	var searchField = $(this).prev('.js-search-field-post');
 	searchField.on('change', function(){
@@ -40,9 +64,50 @@ $('.js-search-btn-post').on('click', function(){
 });
 
 
+// open-close dropdown menu
+$('.js-dropdown-toggle').on('click', function() {
+	$('.js-dropdown-menu').slideToggle(200);
+	return false;
+});
+
+$('.js-dropdown-menu-item').on('click', function() {
+	$('.js-dropdown-menu').slideUp(200);
+	return false;
+});
 
 
+//set check when click on message-item
+$('.message').on('click', function() {
+	var checkbox = $(this).find('input[type="checkbox"]');
+		if(checkbox.is(':checked')) {
+			checkbox.prop('checked', false);
+			$(this).removeClass('checked');
+			$('.footer-buttons').removeClass('show');
+		} else {
+			checkbox.prop('checked', true);
+			$(this).addClass('checked');
+			$('.footer-buttons').addClass('show');
+		}
+});
 
+
+//open-close footer menu
+$('.footer-button-close').on('click', function() {
+	$('.footer-buttons').removeClass('show');
+});
+
+
+//delete post when click to delete button
+$('.footer-button-delete').on('click', function() {
+
+	$('.message').each(function(){
+		if( $(this).hasClass('checked') ) {
+			$(this).remove();
+		}
+	});
+
+	$('.footer-buttons').removeClass('show');
+});
 
 
 });
